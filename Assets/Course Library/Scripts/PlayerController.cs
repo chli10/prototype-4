67f946a3935a0d77b3c12f4043e3bea80807e7e0
chli10 <121971857+chli10@users.noreply.github.com>
2023-03-14 14:19:32 -0700

    made powerup indicator active not active depending on powerup or not

--- Assets/Course Library/Scripts/PlayerController.cs
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -10,7 +10,8 @@ public class PlayerController : MonoBehaviour
     private GameObject focalPoint;
     private float powerUpStrength = 15.0f;
     public float speed = 0.2f;
-    public bool hasPowerup = false; 
+    public bool hasPowerup = false;
+    public GameObject powerupIndicator;
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +27,7 @@ public class PlayerController : MonoBehaviour
 
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
 
+        powerupIndicator.transform.position = transform.position + new Vector3(0,-0.5f,0); 
     }
 
     private void OnTriggerEnter(Collider other)
@@ -33,6 +35,7 @@ public class PlayerController : MonoBehaviour
         if(other.CompareTag("Powerup"))
         {
             hasPowerup = true;
+            powerupIndicator.gameObject.SetActive(true);
             Destroy(other.gameObject);
             StartCoroutine(PowerupCountdownRoutine());
         }
@@ -42,7 +45,8 @@ public class PlayerController : MonoBehaviour
     IEnumerator PowerupCountdownRoutine()
     {
         yield return new WaitForSeconds(7);
-        hasPowerup = false; 
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
     }
     private void OnCollisionEnter(Collision collision)
     {
